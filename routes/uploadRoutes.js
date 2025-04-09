f4f924fa9a8b1d35ad00ceed4153c4fd2a137e82
Kelvin Murillo <kelvinmurillo001@gmail.com>
2025-04-08 23:14:19 -0500

    🛠️ Fix: acualizaciones del proyecto

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const cloudinary = require('../config/cloudinary');
+const { cloudinary } = require('../config/cloudinary');
 const streamifier = require('streamifier');
 const authMiddleware = require('../middleware/authMiddleware');
 // Opcional: solo permitir admins
