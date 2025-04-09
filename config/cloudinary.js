9be3644de0b7fb27a1da6e7e9756a29702a47aca
Kelvin Murillo <kelvinmurillo001@gmail.com>
2025-04-08 22:45:41 -0500

    🛠️ Fix: acualizaciones del proyecto

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -2,9 +2,9 @@ const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
-  cloud_name: dmbnkrhek,
-  api_key:    831978565358562,
-  api_secret: TIGp2ZEOr-oqPWG6K9V9Wg-gcWSU
+  cloud_name: 'dmbnkrhek',
+  api_key:    '831978565358562',
+  api_secret: 'TIGp2ZEOr-oqPWG6K9V9Wg-gcWSU'
 });
 
 const storage = new CloudinaryStorage({
