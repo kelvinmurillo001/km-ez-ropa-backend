3816f7623884e0eb0f08b8fef692643df3566c99
Kelvin Murillo <kelvinmurillo001@gmail.com>
2025-04-19 07:15:44 -0500

    🛠️ Fix: acualizaciones del proyecto

--- controllers/product/index.js
+++ controllers/product/index.js
@@ -1,13 +1,13 @@
-// controllers/product/index.js
-
 const getAllProducts = require('./getAllProducts');
 const getProductById = require('./getProductById');
+const createProduct = require('./createProduct'); // ✅ agregar esta línea
 const updateProduct = require('./updateProduct');
 const deleteProduct = require('./deleteProduct');
 
 module.exports = {
   getAllProducts,
   getProductById,
+  createProduct, // ✅ y esta también
   updateProduct,
   deleteProduct
 };
