0fba29de082586996b7b6ef7e19eb6917c4f47fd
Kelvin Murillo <kelvinmurillo001@gmail.com>
2025-05-06 06:32:03 -0500

    feat: agregar ruta temporal para crear admin

--- controllers/product/index.js
+++ controllers/product/index.js
@@ -1,16 +1,18 @@
 // 📁 backend/controllers/products/index.js
-import getAllProducts from './getAllProducts.js'
-import getProductById from './getProductById.js'
-import createProduct from './createProduct.js'
-import updateProduct from './updateProduct.js'
-import deleteProduct from './deleteProduct.js'
-import getProductBySlug from './getProductBySlug.js' // ✅ NUEVO
 
+import getAllProducts from './getAllProducts.js';
+import getProductById from './getProductById.js';
+import createProduct from './createProduct.js';
+import updateProduct from './updateProduct.js';
+import deleteProduct from './deleteProduct.js';
+import getProductBySlug from './getProductBySlug.js'; // ✅ NUEVO
+
+// 📦 Exportación agrupada para facilitar el mantenimiento y la importación
 export {
   getAllProducts,
   getProductById,
   createProduct,
   updateProduct,
   deleteProduct,
-  getProductBySlug // ✅ NUEVO
-}
+  getProductBySlug
+};
